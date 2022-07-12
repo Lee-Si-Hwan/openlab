9847b798f54d8fe262b995944efd56ec81cd654f
double J, T <>
2022-07-12 17:08:39 +0900

    사진 업로드
    ㅇㅇ

--- npy_save_hsv.py
+++ npy_save_hsv.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os, glob, cv2, numpy as np
 from sklearn.model_selection import train_test_split
 
-groups_folder_path = 'C:/Users/user/Desktop/colorgroup'
+#groups_folder_path = 'C:/Users/user/Desktop/colorgroup'
 categories = ["red","blue","brown","orange","green","yellow"]
 nb_classes = len(categories) #카테고리갯수: 6개
 
@@ -16,32 +16,19 @@ H = []
 for idx, cat in enumerate(categories):
     
     #one-hot 돌리기.
-    label = [0 for i in range(nb_classes)] #one-hot으로 라벨을 붙혀줌
-    label[idx] = 1
 
-    image_dir = groups_folder_path + "/" + cat
-    files = glob.glob(image_dir+"/*.jpg")
-    print(cat, " 파일 길이 : ", len(files))
-    for i, f in enumerate(files):
-        img = Image.open(f)      #폴더를 열어주고 이미지를 읽음
-        img = img.convert("RGB")   #RGB로 바꿈
-        img = img.resize((image_w, image_h)) #이미지 크기를 모두 학습시키기 쉽게 600x600크기고
-        data = np.asarray(img) #숫자로
-        src = cv2.imread(f, cv2.IMREAD_COLOR)
-        hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
-        h, s, v = cv2.split(hsv)
-        print(type(h))
-        print(type(data))
-        #for i in range(600):
-            #for j in range(600):
-                #data[i,j,0]=h[i,j]
-        #print(data)
+    image_dir = "C:/Users/csi2/Documents/GitHub/openlab/img_captured.png"
+    src = cv2.imread(image_dir, cv2.IMREAD_COLOR)
+    hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    print(type(h), h[:100])
         
         
 
-        X.append(data) #리스트에 추가 
-        y.append(label)
-        H.append(h)
+    X.append(data) #리스트에 추가 
+    y.append(label)
+    H.append(h)
+    break
 
 print(H)
 print(X)
